f1e98582a54e16082283e2d88d5d403fb651f9cf
Robert Citek <robert.citek@gmail.com>
2010-03-20 02:37:10 -0400

    added second data file
    added mkdir

--- sample.sh
+++ sample.sh
@@ -2,15 +2,19 @@
 
 # creates a sample data file for git to work with
 
+mkdir sample
+cd sample
 git init
-touch data.txt
-git add data.txt
+touch data1.txt data2.txt
+git add data1.txt data2.txt
 git commit -a -m "initial commit"
 echo "the log after one addition ..."
 git log --pretty=oneline
 for i in {1..10} ; do
-  echo "item $i" >> data.txt
-  git commit -a -m "add item $i"
+  echo "item $i" >> data1.txt
+  git commit -a -m "add item $i to data1.txt"
+  echo "copy item $i" >> data2.txt
+  git commit -a -m "add item $i to data2.txt"
 done > /dev/null
 echo "the log after several addition ..."
 git log --pretty=oneline
