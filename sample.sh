21a35efb07330777895032a0cf266fdf567ba2c4
Robert Citek <robert.citek@gmail.com>
2010-03-30 11:43:14 -0400

    changed folder from sample to myProj
    removed displaying the logs

--- sample.sh
+++ sample.sh
@@ -6,23 +6,23 @@
 
 # create a folder in which to work
 [ -d sample ] && { echo "sample directory already exists" ; exit 1 ; }
-mkdir sample
-cd sample
+mkdir myProj
+cd myProj
 
 # initialize repository
 git init
 touch data1.txt data2.txt
 git add data1.txt data2.txt
 git commit -a -m "initial commit"
-echo "the log after one addition ..."
-git log --pretty=oneline
 
 # edit, commit cycle
-for i in {1..4} ; do
+for i in {1..10} ; do
   echo "item $i" >> data1.txt
   git commit -a -m "add item $i to data1.txt"
   echo "copy item $i" >> data2.txt
   git commit -a -m "add item $i to data2.txt"
 done > /dev/null
-echo "the log after several addition ..."
-git log --pretty=oneline
+
+
+
+
